10c6455e9ec6041208997fea88e01131b99c45d0
jyoti92-korde <117429213+jyoti92-korde@users.noreply.github.com>
2023-02-21 14:05:59 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit';
+ import {Provider} from 'react-redux';
+ import airReducer from './reducers/airReducer';
+ import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas/rootSaga';
+
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer:airReducer,
+  middleware:[sagaMiddleware]
+});
+
+sagaMiddleware.run(rootSaga )
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
   </React.StrictMode>
 );
 
